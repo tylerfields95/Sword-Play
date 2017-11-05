00fd1555ebfa09929073f58953dbba09cac5b5ea
HaveACigar <32943283+HaveACigar@users.noreply.github.com>
2017-11-04 22:53:40 -0400

    Add files via upload
    These C# files are for enemy health and enemy movement. I'm gonna try and add the damage indicator (turning the enemies red on sword hit) tomorrow.

---
+++ Assets/Assets/AttackPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour {
+
+    private Transform player;
+	void Start () {
+        player = GameObject.Find("Test sword").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, 2 * Time.deltaTime);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Collider>().name == "Test sword")
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
