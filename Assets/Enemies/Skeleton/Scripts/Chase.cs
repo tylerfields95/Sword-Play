145f1dca93b6629fd5cfb20428a43a10559f370e
Jack <Jrussle@emich.edu>
2017-11-13 21:04:17 -0500

    add second perry animation, make skeleton stop when hit, make skeleton have knockback on being damaged

--- Assets/Enemies/Skeleton/Scripts/Chase.cs
+++ Assets/Enemies/Skeleton/Scripts/Chase.cs
@@ -5,20 +5,32 @@ public class Chase : MonoBehaviour {
 
 	public Transform player;
 	private Animator anim;
+	private int in_combat;
 	float speed = .5f;
+	detectHit hit;
+	
 
 
 
 	// Use this for initialization
 	void Start () 
 	{
+
 		anim = GetComponent<Animator>();
+		in_combat = 0;
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		hit = gameObject.GetComponentInChildren<detectHit>();
+		if(hit.parry == true){
+			in_combat = 50;
+			hit.parry = false;
+		}
+		in_combat--;
+		Debug.Log("in combat = "+ in_combat);
 		 CharacterController controller = GetComponent<CharacterController>();
 
 		Vector3 direction = player.position - this.transform.position;
@@ -34,63 +46,77 @@ public class Chase : MonoBehaviour {
 			
 			Vector3 movement = direction*speed;
 		
+		
+				anim.SetBool("is_idle",false);
+				if(direction.magnitude > 3 && in_combat <0)
+				{
+					Debug.Log("is Moving");
+					 movement.y -= 20.0f * Time.deltaTime;
+	
+					controller.Move(movement * Time.deltaTime);
+					anim.SetBool("is_walking",true);
+					anim.SetBool("is_attacking",false);
+					anim.SetBool("is_damaged",false);
+					anim.SetBool("is_block1",false);
+					anim.SetBool("is_attack2",false);
+											
+				}
+				else
+				{
+					
+					int rando = Random.Range(1,4);
+					if(rando==1){
+					
+					//delay for animation before skeleton can walk
+							if(in_combat<0){
+							in_combat = 200;
+							}
+												anim.SetBool("is_attacking",true);
+							anim.SetBool("is_walking",false);
+							anim.SetBool("is_damaged",false);
+							anim.SetBool("is_block1",false);
+							anim.SetBool("is_attack2",false);
+					}
+					else if(rando ==2){
+						//delay for animation before skeleton can walk
+							if(in_combat<0){
+							in_combat = 200;
+							}
 
-			anim.SetBool("is_idle",false);
-			if(direction.magnitude > 3)
+							anim.SetBool("is_attacking",false);
+							anim.SetBool("is_walking",false);
+							anim.SetBool("is_damaged",false);
+							anim.SetBool("is_block1",true);
+							anim.SetBool("is_attack2",false);
+						
+					}
+					else{
+						//delay for animation before skeleton can walk
+							if(in_combat<0){
+							in_combat = 150;
+							}
+							anim.SetBool("is_attacking",false);
+							anim.SetBool("is_walking",false);
+							anim.SetBool("is_damaged",false);
+							anim.SetBool("is_block1",false);
+							anim.SetBool("is_attack2",true);
+						
+					}
+	
+				}
+	
+			}
+			else 
 			{
-				 movement.y -= 20.0f * Time.deltaTime;
-
-				controller.Move(movement * Time.deltaTime);
-				anim.SetBool("is_walking",true);
-				anim.SetBool("is_attacking",false);
+				anim.SetBool("is_idle", true);
+				anim.SetBool("is_walking", false);
+				anim.SetBool("is_attacking", false);
 				anim.SetBool("is_damaged",false);
 				anim.SetBool("is_block1",false);
 				anim.SetBool("is_attack2",false);
-										
+				
 			}
-			else
-			{
-				int rando = Random.Range(1,4);
-				if(rando==1){
-
-					
-				anim.SetBool("is_attacking",true);
-						anim.SetBool("is_walking",false);
-						anim.SetBool("is_damaged",false);
-						anim.SetBool("is_block1",false);
-						anim.SetBool("is_attack2",false);
-				}
-				else if(rando ==2){
-
-						anim.SetBool("is_attacking",false);
-						anim.SetBool("is_walking",false);
-						anim.SetBool("is_damaged",false);
-						anim.SetBool("is_block1",true);
-						anim.SetBool("is_attack2",false);
-					
-				}
-				else{
-					anim.SetBool("is_attacking",false);
-						anim.SetBool("is_walking",false);
-						anim.SetBool("is_damaged",false);
-						anim.SetBool("is_block1",false);
-						anim.SetBool("is_attack2",true);
-					
-				}
-
-			}
-
-		}
-		else 
-		{
-			anim.SetBool("is_idle", true);
-			anim.SetBool("is_walking", false);
-			anim.SetBool("is_attacking", false);
-			anim.SetBool("is_damaged",false);
-			anim.SetBool("is_block1",false);
-			anim.SetBool("is_attack2",false);
-			
-		}
+		
 
 	}
 	void OnTriggerEnter(Collider other){
@@ -98,11 +124,17 @@ public class Chase : MonoBehaviour {
 		
 		if(other.name=="Cube"){
 			anim.Play("Damage",0, 0.0f);
+			in_combat = 50;
 			anim.SetBool("is_idle", false);
 			anim.SetBool("is_walking", false);
 			anim.SetBool("is_attacking", false);
 			anim.SetBool("is_block1",false);
 			anim.SetBool("is_attack2",false);
+			CharacterController controller = transform.root.GetComponent<CharacterController>();
+			Vector3 direction = player.position - transform.position;
+			direction.y=0;
+			direction = direction*-2.0f;
+			controller.Move(direction * 10*Time.deltaTime);
 
 			
 		}
--- Assets/Enemies/Skeleton/Scripts/detectHit.cs
+++ Assets/Enemies/Skeleton/Scripts/detectHit.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class detectHit : MonoBehaviour {
 	private Animator anim;
 	public Transform player;
+	public bool parry;
+	
 	void OnTriggerEnter(Collider other){
 
 
 		if(other.name=="Cube"){
 			Debug.Log("PARRY/BLOCK");
 			GetBack();
-			anim.Play("Attack1Parry",0, 0.0f);
-			anim.SetBool("is_idle", false);
-			anim.SetBool("is_walking", false);
-			anim.SetBool("is_attacking", false);
-			anim.SetBool("is_block1",false);
-			anim.SetBool("is_attack2",false);
+			anim.SetTrigger("is_blocked");
+		
 
 			
 		
@@ -39,7 +37,7 @@ public class detectHit : MonoBehaviour {
 		Vector3 direction = player.position - transform.root.transform.position;
 		Debug.Log(direction);
 		direction.y=0;
-		direction = direction*-2.0f;
+		direction = direction*-1.0f;
 				Debug.Log("2: "+direction);
 		controller.Move(direction * 10*Time.deltaTime);
 					Debug.Log("they should have moved");			
